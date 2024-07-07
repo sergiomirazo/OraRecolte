250fb4032a3209f009b95dcf7cd02ee87a7025c4
sergiomirazo <sergiomisael91@gmail.com>
2024-07-06 23:16:37 -0700

    update styles and content

--- js/custom.js
+++ js/custom.js
@@ -37,7 +37,7 @@ $('.slick_slider_nav').slick({
 
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
+        center: new google.maps.LatLng(29.0884798,-110.9509581),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
