91da6e74693b1d716bc827505dcb91ea1da7de9c
sergiomirazo <sergiomisael91@gmail.com>
2024-07-14 13:53:29 -0700

    abrazaderas_data.json and abrazaderas images added

--- js/custom.js
+++ js/custom.js
@@ -8,96 +8,139 @@ function getYear() {
 getYear();
 
 
-$('.custom_slick_slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: true,
-    fade: true,
-    adaptiveHeight: true,
-    asNavFor: '.slick_slider_nav',
-    responsive: [{
-        breakpoint: 768,
-        settings: {
-            dots: false
-        }
-    }]
-})
-
-$('.slick_slider_nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.custom_slick_slider',
-    centerMode: false,
-    focusOnSelect: true,
-    variableWidth: true
-});
-
-
-/** google_map js **/
-
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(29.0884798,-110.9509581),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
-
 // valves modal window
 
 $(document).ready(function() {
-    let valvulasPerPage = 10;
-    let currentPage = 1;
+    let itemsPerPage = 10;
+    
+    // Válvulas
+    let currentPageValvulas = 1;
     let valvulasData = [];
 
-    // Load valvulas_data.json
-    $.getJSON('/js/valvulas_data.json', function(data) {
-        valvulasData = data;
-        renderValvulas(valvulasData, currentPage, valvulasPerPage);
-        renderPagination(valvulasData, valvulasPerPage);
-    });
+    $.getJSON('/js/valvulas_data.json')
+        .done(function(data) {
+            console.log('JSON de Válvulas Cargado Correctamente:', data);
+            valvulasData = data;
+            renderItems(valvulasData, currentPageValvulas, itemsPerPage, '#valvulas-content');
+            renderPagination(valvulasData, itemsPerPage, '#pagination-valvulas');
+        })
+        .fail(function(jqxhr, textStatus, error) {
+            let err = textStatus + ', ' + error;
+            console.log('Fallo en la carga del JSON de Válvulas: ' + err);
+        });
 
-    function renderValvulas(data, page, itemsPerPage) {
+    // Medidores
+    let currentPageMedidores = 1;
+    let medidoresData = [];
+
+    $.getJSON('/js/medidores_data.json')
+        .done(function(data) {
+            console.log('JSON de Medidores Cargado Correctamente:', data);
+            medidoresData = data;
+            renderItems(medidoresData, currentPageMedidores, itemsPerPage, '#medidores-content');
+            renderPagination(medidoresData, itemsPerPage, '#pagination-medidores');
+        })
+        .fail(function(jqxhr, textStatus, error) {
+            let err = textStatus + ', ' + error;
+            console.log('Fallo en la carga del JSON de Medidores: ' + err);
+        });
+
+    // Bombas
+    let currentPageBombas = 1;
+    let bombasData = [];
+
+    $.getJSON('/js/bombas_data.json')
+        .done(function(data) {
+            console.log('JSON de Bombas Cargado Correctamente:', data);
+            bombasData = data;
+            renderItems(bombasData, currentPageBombas, itemsPerPage, '#bombas-content');
+            renderPagination(bombasData, itemsPerPage, '#pagination-bombas');
+        })
+        .fail(function(jqxhr, textStatus, error) {
+            let err = textStatus + ', ' + error;
+            console.log('Fallo en la carga del JSON de Bombas: ' + err);
+        });
+
+    // Filtros
+    let currentPageFiltros = 1;
+    let filtrosData = [];
+
+    $.getJSON('/js/filtros_data.json')
+        .done(function(data) {
+            console.log('JSON de Filtros Cargado Correctamente:', data);
+            filtrosData = data;
+            renderItems(filtrosData, currentPageFiltros, itemsPerPage, '#filtros-content');
+            renderPagination(filtrosData, itemsPerPage, '#pagination-filtros');
+        })
+        .fail(function(jqxhr, textStatus, error) {
+            let err = textStatus + ', ' + error;
+            console.log('Fallo en la carga del JSON de Filtros: ' + err);
+        });
+
+    function renderItems(data, page, itemsPerPage, containerSelector) {
         let content = '';
         let start = (page - 1) * itemsPerPage;
         let end = start + itemsPerPage;
         let paginatedItems = Object.keys(data).slice(start, end);
 
+        console.log('Elementos Paginados:', paginatedItems);
+
         $.each(paginatedItems, function(index, key) {
-            let valvula = data[key];
+            let item = data[key];
+            let imgPath = containerSelector.includes('valvulas') ? 'valvulas' 
+                         : containerSelector.includes('medidores') ? 'medidores' 
+                         : containerSelector.includes('bombas') ? 'bombas' 
+                         : 'filtros';
             content += `
-                <div class="valvula-item">
-                    <img src="/images/valvulas/${key}.png" alt="${valvula.title}" class="img-fluid">
-                    <h3>${valvula.title}</h3>
-                    <figcaption>${valvula.figcaption}</figcaption>
+                <div class="item">
+                    <img width="200" src="/images/${imgPath}/${key}.png" alt="${item.title}" class="img-fluid">
+                    <h3 style="color: var(--secondary);">${item.title}</h3>
+                    <p>${containerSelector.includes('valvulas') ? item.figcaption : ''}</p>
                 </div>
-                <hr>
+                <hr style="color: var(--primary);">
             `;
+            console.log('Agregar ítem:', item);
         });
 
-        $('#valvulas-content').html(content);
+        $(containerSelector).html(content);
+        console.log('Contenido Final:', content);
     }
 
-    function renderPagination(data, itemsPerPage) {
+    function renderPagination(data, itemsPerPage, paginationSelector) {
         let pageCount = Math.ceil(Object.keys(data).length / itemsPerPage);
         let paginationContent = '';
 
         for (let i = 1; i <= pageCount; i++) {
             paginationContent += `
-                <li class="page-item ${i === currentPage ? 'active' : ''}">
+                <li class="page-item ${i === (paginationSelector.includes('valvulas') ? currentPageValvulas : paginationSelector.includes('medidores') ? currentPageMedidores : paginationSelector.includes('bombas') ? currentPageBombas : currentPageFiltros) ? 'active' : ''}">
                     <a class="page-link" href="#" data-page="${i}">${i}</a>
                 </li>
             `;
         }
 
-        $('#pagination').html(paginationContent);
+        $(paginationSelector).html(paginationContent);
     }
 
-    // Pagination click event
-    $('#pagination').on('click', '.page-link', function(e) {
+    // Manejar clics en paginación
+    $('#pagination-valvulas, #pagination-medidores, #pagination-bombas, #pagination-filtros').on('click', '.page-link', function(e) {
         e.preventDefault();
-        currentPage = $(this).data('page');
-        renderValvulas(valvulasData, currentPage, valvulasPerPage);
-        renderPagination(valvulasData, valvulasPerPage);
+        let page = $(this).data('page');
+        if ($(this).parents('#pagination-valvulas').length) {
+            currentPageValvulas = page;
+            renderItems(valvulasData, currentPageValvulas, itemsPerPage, '#valvulas-content');
+            renderPagination(valvulasData, itemsPerPage, '#pagination-valvulas');
+        } else if ($(this).parents('#pagination-medidores').length) {
+            currentPageMedidores = page;
+            renderItems(medidoresData, currentPageMedidores, itemsPerPage, '#medidores-content');
+            renderPagination(medidoresData, itemsPerPage, '#pagination-medidores');
+        } else if ($(this).parents('#pagination-bombas').length) {
+            currentPageBombas = page;
+            renderItems(bombasData, currentPageBombas, itemsPerPage, '#bombas-content');
+            renderPagination(bombasData, itemsPerPage, '#pagination-bombas');
+        } else {
+            currentPageFiltros = page;
+            renderItems(filtrosData, currentPageFiltros, itemsPerPage, '#filtros-content');
+            renderPagination(filtrosData, itemsPerPage, '#pagination-filtros');
+        }
     });
 });
